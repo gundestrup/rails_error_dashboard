11595e81680194df8f3db80b663a3b12b9a723c0
Anjan Jagirdar <anjan.jagirdar@gmail.com>
2025-12-27 10:24:40 +0530

    fix: change schema version from 8.0 to 7.0 for Rails 7.0 compatibility
    The schema.rb was generated with Rails 8.x which creates
    ActiveRecord::Schema[8.0], but Rails 7.0 doesn't recognize
    version 8.0 (only supports up to 7.0).
    
    This change uses ActiveRecord::Schema[7.0] which is:
    - Compatible with Rails 7.0, 7.1, 7.2 (native support)
    - Compatible with Rails 8.0, 8.1 (backwards compatible)
    - The lowest common denominator for our Appraisal test matrix
    
    This fixes the CI failure:
      ArgumentError: Unknown migration version "8.0";
      expected one of "4.2", "5.0", "5.1", "5.2", "6.0", "6.1", "7.0"

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_26_020100) do
+ActiveRecord::Schema[7.0].define(version: 2025_12_26_020100) do
   create_table "rails_error_dashboard_cascade_patterns", force: :cascade do |t|
     t.integer "parent_error_id", null: false
     t.integer "child_error_id", null: false
